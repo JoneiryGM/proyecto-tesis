77a3d9471b70d03eeed251751c08b9704f48fddd
Reimy21 <reimyrick@gmail.com>
2026-04-19 20:52:22 -0400

    Configurar gitignore y limpiar archivos temporales

--- Api_Eden/Services/TratamientoService/Interface/IFallecimientoService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Api_Eden.DTOs.MedicoDto;
-
-namespace Api_Eden.Services.TratamientoService.Interface
-{
-    public interface IFallecimientoService
-    {
-        Task<(bool ok, string mensaje)> RegistrarFallecimiento(RegistrarFallecimientoDto dto);
-    }
-}
